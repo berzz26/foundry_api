d648f2580454cac9eeae6cc65292784abaddb1e1
berzz26 <aumtamboli2233@gmail.com>
2026-06-11 07:21:33 +0000

    feat: attach routes in main

--- cmd/api.go
+++ cmd/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/berzz26/foundry_api/internal/companies"
 	"github.com/berzz26/foundry_api/internal/users"
 	"github.com/berzz26/foundry_api/pkg/config"
 	"github.com/berzz26/foundry_api/pkg/database"
@@ -23,12 +24,17 @@ func main() {
 	userService := users.NewService(userRepo)
 	userHandler := users.NewHandler(userService)
 
+	companyRepo := companies.NewRepository(db.DB)
+	companyService := companies.NewService(companyRepo)
+	companyHandler := companies.NewHandler(companyService)
+
 	app := fiber.New()
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
 	
 	//mount the routes
 	v1.Mount("/users", userHandler.SetupRoutes())
+	v1.Mount("/companies", companyHandler.SetupRoutes())
 
 	log.Fatal(app.Listen(":" + cfg.HTTPPort))
 

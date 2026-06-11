d017ca32dc070c5c2f1f384ccc2a484a2965fa02
berzz26 <aumtamboli2233@gmail.com>
2026-06-11 06:09:41 +0000

    feat: implement required repository functions

--- internal/users/repository.go
+++ internal/users/repository.go
@@ -35,10 +35,10 @@ func (r *Repository) GetByID(ctx context.Context, id string) (*User, error) {
 	return &user, nil
 }
 
-func (r *Repository)AddUser(ctx context.Context, user *User) (*User, error){
+func (r *Repository) AddUser(ctx context.Context, user *User) (*User, error) {
 	var addedUser User
-	
-	err:= r.db.QueryRow(ctx, `
+
+	err := r.db.QueryRow(ctx, `
 	INSERT INTO users (id, email, firstName, lastName, passwordHash, profileImageUrl, provider, providerId, createdAt, updatedAt) 
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
 	RETURNING id, email, firstName, lastName, passwordHash, profileImageUrl, provider, providerId, createdAt, updatedAt
@@ -58,4 +58,98 @@ func (r *Repository)AddUser(ctx context.Context, user *User) (*User, error){
 		return nil, err
 	}
 	return &addedUser, nil
+}
+
+func (r *Repository) GetByEmail(ctx context.Context, email string) (*User, error) {
+	var user User
+
+	err := r.db.QueryRow(ctx, `
+	SELECT id, email, firstName, lastName, passwordHash, profileImageUrl, provider, providerId, createdAt, updatedAt 
+	FROM users 
+	WHERE email = $1
+	`, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.PasswordHash,
+		&user.ProfileImageURL,
+		&user.Provider,
+		&user.ProviderID,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *Repository) List(ctx context.Context, limit int, offset int) ([]User, error) {
+	var users []User
+
+	rows, err := r.db.Query(ctx, `
+	SELECT id, email, firstName, lastName, passwordHash, profileImageUrl, provider, providerId, createdAt, updatedAt 
+	FROM users 
+	LIMIT $1 OFFSET $2`, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(
+			&user.ID,
+			&user.Email,
+			&user.FirstName,
+			&user.LastName,
+			&user.PasswordHash,
+			&user.ProfileImageURL,
+			&user.Provider,
+			&user.ProviderID,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
+func (r *Repository) Update(ctx context.Context, user *User) (*User, error) {
+	var updatedUser User
+
+	err := r.db.QueryRow(ctx, `
+	UPDATE users SET 
+	email = $1, firstName = $2, lastName = $3, passwordHash = $4, profileImageUrl = $5, provider = $6, providerId = $7, createdAt = $8, updatedAt = $9 
+	WHERE id = $10
+	RETURNING id, email, firstName, lastName, passwordHash, profileImageUrl, provider, providerId, createdAt, updatedAt
+	`, user.Email, user.FirstName, user.LastName, user.PasswordHash, user.ProfileImageURL, user.Provider, user.ProviderID, user.CreatedAt, user.UpdatedAt, user.ID).Scan(
+		&updatedUser.ID,
+		&updatedUser.Email,
+		&updatedUser.FirstName,
+		&updatedUser.LastName,
+		&updatedUser.PasswordHash,
+		&updatedUser.ProfileImageURL,
+		&updatedUser.Provider,
+		&updatedUser.ProviderID,
+		&updatedUser.CreatedAt,
+		&updatedUser.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedUser, nil
+}
+
+func (r *Repository) Delete(ctx context.Context, id string) error {
+	_, err := r.db.Exec(ctx, `
+	DELETE FROM users WHERE id = $1
+	`, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }

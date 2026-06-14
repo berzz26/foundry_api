a1d4474b0c85cecc48f6a6c61d881b2ebba97903
berzz26 <aumtamboli2233@gmail.com>
2026-06-14 15:55:34 +0000

    feat: update Company model and response DTO to include source logo fields

--- internal/companies/dto.go
+++ internal/companies/dto.go
@@ -3,29 +3,31 @@ package companies
 import "time"
 
 type CompanyResponseDTO struct {
-	ID                int64      `json:"id"`
-	Name              string     `json:"name"`
-	Slug              string     `json:"slug"`
-	Website           *string    `json:"website,omitempty"`
-	Tagline           *string    `json:"tagline,omitempty"`
-	Description       *string    `json:"description,omitempty"`
-	HiringDescription *string    `json:"hiringDescription,omitempty"`
-	TechStack         *string    `json:"techStack,omitempty"`
-	Batch             *string    `json:"batch,omitempty"`
-	Stage             *string    `json:"stage,omitempty"`
-	TeamSize          *int32     `json:"teamSize,omitempty"`
-	Location          *string    `json:"location,omitempty"`
-	ParentSector      *string    `json:"parentSector,omitempty"`
-	ChildSector       *string    `json:"childSector,omitempty"`
-	Industry          *string    `json:"industry,omitempty"`
-	LogoURL           *string    `json:"logoUrl,omitempty"`
-	SmallLogoURL      *string    `json:"smallLogoUrl,omitempty"`
-	Country           *string    `json:"country,omitempty"`
-	FoundedAt         *time.Time `json:"foundedAt,omitempty"`
-	LinkedinURL       *string    `json:"linkedinUrl,omitempty"`
-	TwitterURL        *string    `json:"twitterUrl,omitempty"`
-	CreatedAt         time.Time  `json:"createdAt"`
-	UpdatedAt         time.Time  `json:"updatedAt"`
+	ID                 int64      `json:"id"`
+	Name               string     `json:"name"`
+	Slug               string     `json:"slug"`
+	Website            *string    `json:"website,omitempty"`
+	Tagline            *string    `json:"tagline,omitempty"`
+	Description        *string    `json:"description,omitempty"`
+	HiringDescription  *string    `json:"hiringDescription,omitempty"`
+	TechStack          *string    `json:"techStack,omitempty"`
+	Batch              *string    `json:"batch,omitempty"`
+	Stage              *string    `json:"stage,omitempty"`
+	TeamSize           *int32     `json:"teamSize,omitempty"`
+	Location           *string    `json:"location,omitempty"`
+	ParentSector       *string    `json:"parentSector,omitempty"`
+	ChildSector        *string    `json:"childSector,omitempty"`
+	Industry           *string    `json:"industry,omitempty"`
+	LogoURL            *string    `json:"sourceLogoUrl,omitempty"`
+	SourceLogoURL      *string    `json:"logoUrl,omitempty"`
+	SmallLogoURL       *string    `json:"smallLogoUrl,omitempty"`
+	SourceSmallLogoURL *string    `json:"sourceSmallLogoUrl,omitempty"`
+	Country            *string    `json:"country,omitempty"`
+	FoundedAt          *time.Time `json:"foundedAt,omitempty"`
+	LinkedinURL        *string    `json:"linkedinUrl,omitempty"`
+	TwitterURL         *string    `json:"twitterUrl,omitempty"`
+	CreatedAt          time.Time  `json:"createdAt"`
+	UpdatedAt          time.Time  `json:"updatedAt"`
 }
 
 type CompanyFilters struct {

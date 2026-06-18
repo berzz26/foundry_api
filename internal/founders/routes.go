be9668c69a3dc9374a3507d5f4fa0f5007841ecd
berzz26 <aumtamboli2233@gmail.com>
2026-06-18 05:39:55 +0000

    feat: no need to "create" the founder

--- internal/founders/routes.go
+++ internal/founders/routes.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func (h *Handler) SetupRoutes() *fiber.App {
 	app := fiber.New()
 
-	app.Post("/", h.Create)
+	
 	app.Get("/", h.List)
 	app.Get("/:id", h.GetByID)
 	app.Put("/:id", h.Update)
